ba3929147c41d28910c215f39e18e3e3c71c35fd
Toon Willems <willemstoon@gmail.com>
2013-09-27 13:50:50 +0200

    whitespace removal

--- Isengard/config/routes.rb
+++ Isengard/config/routes.rb
@@ -40,7 +40,7 @@ Isengard::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
