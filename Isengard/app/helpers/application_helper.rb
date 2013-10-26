1596c971e42d062138b902567a6a71d235c34a38
Tom Naessens <siloxtom@gmail.com>
2013-10-26 23:53:12 +0200

    Implemented the form helpers

--- Isengard/app/helpers/application_helper.rb
+++ Isengard/app/helpers/application_helper.rb
@@ -4,4 +4,16 @@ module ApplicationHelper
     render partial: "form_errors", locals: {object: object}
   end
 
+  def form_text_field(f, tag)
+    render partial: "form_text_field", locals: {f: f, tag: tag}
+  end
+
+  def form_date_field(f, tag, id, value)
+    render partial: "form_date_field", locals: {f: f, tag: tag, id: id, value: value}
+  end
+
+  def form_text_area(f, tag)
+    render partial: "form_text_area", locals: {f: f, tag: tag}
+  end
+
 end
