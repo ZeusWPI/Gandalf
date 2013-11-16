774d26716238bfa7d9d95adcc48a11804ca61b61
Tom Naessens <siloxtom@gmail.com>
2013-11-16 18:30:30 +0100

    All edit stuff is on one page now (fixes #123)

--- Isengard/app/controllers/events_controller.rb
+++ Isengard/app/controllers/events_controller.rb
@@ -12,7 +12,6 @@ class EventsController < ApplicationController
       @events += Event.accessible_by(current_ability).to_a
     end
     @events.uniq!
-
   end
 
   def show
@@ -30,25 +29,19 @@ class EventsController < ApplicationController
     redirect_to action: :index
   end
 
-  def registration_times
-    @event = Event.find params.require(:event_id)
-    authorize! :update, @event
-    if @event.update params.require(:event).permit(:registration_close_date, :registration_open_date, :show_ticket_count)
-      flash[:notice] = "succesfully updated registration times"
-    else
-      flash[:error] = "something went wrong"
-    end
-    respond_with @event
-  end
-
   def update
-    @event.update params.require(:event).permit(:name, :organisation, :bank_number, :location, :website, :start_date, :end_date, :description)
-    respond_with @event
+    authorize! :update, @event
+
+    @event.update params.require(:event).permit(:name, :organisation, :location, :website, :start_date, :end_date, :description, :bank_number, :registration_close_date, :registration_open_date, :show_ticket_count)
+
+    render action: :edit
   end
 
   def create
     authorize! :create, Event
-    @event = Event.create(params.require(:event).permit(:name, :organisation, :location, :bank_number, :website, :start_date, :end_date, :description).merge club: current_user.club)
+
+    @event = Event.create(params.require(:event).permit(:name, :organisation, :location, :website, :start_date, :end_date, :description).merge club: current_user.club)
+
     respond_with @event
   end
 
