277b0e8a3d027b67717375faa385fbae95124ae0
Toon Willems <willemstoon@gmail.com>
2013-11-16 14:23:28 +0100

    use barby, because we love dolls

--- Isengard/db/schema.rb
+++ Isengard/db/schema.rb
@@ -50,8 +50,8 @@ ActiveRecord::Schema.define(version: 20131114205015) do
     t.string   "club"
     t.datetime "registration_open_date"
     t.datetime "registration_close_date"
-    t.boolean  "show_ticket_count",       default: true
     t.string   "bank_number"
+    t.boolean  "show_ticket_count",       default: true
   end
 
   add_index "events", ["club"], name: "index_events_on_club"
@@ -93,8 +93,8 @@ ActiveRecord::Schema.define(version: 20131114205015) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "event_id"
-    t.integer  "paid"
     t.string   "student_number"
+    t.integer  "paid"
     t.integer  "price"
   end
 
