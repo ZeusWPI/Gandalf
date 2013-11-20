3d1f97ed437384f58283b0492af488f9c5033645
Toon Willems <willemstoon@gmail.com>
2013-11-20 12:30:43 +0100

    add delayed_job

--- Isengard/db/schema.rb
+++ Isengard/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131118133406) do
+ActiveRecord::Schema.define(version: 20131120112719) do
 
   create_table "access_levels", force: true do |t|
     t.string   "name"
@@ -36,6 +36,22 @@ ActiveRecord::Schema.define(version: 20131118133406) do
   add_index "accesses", ["access_level_id"], name: "index_accesses_on_access_level_id"
   add_index "accesses", ["period_id"], name: "index_accesses_on_period_id"
   add_index "accesses", ["registration_id"], name: "index_accesses_on_registration_id"
+
+  create_table "delayed_jobs", force: true do |t|
+    t.integer  "priority",   default: 0, null: false
+    t.integer  "attempts",   default: 0, null: false
+    t.text     "handler",                null: false
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority"
 
   create_table "events", force: true do |t|
     t.string   "name"
