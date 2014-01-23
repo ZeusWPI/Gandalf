a9b61b9e7fa2b307a61343a80e1b19ca82d48550
Tom Naessens <siloxtom@gmail.com>
2014-01-23 18:06:29 +0100

    Revert changes

--- Isengard/app/helpers/application_helper.rb
+++ Isengard/app/helpers/application_helper.rb
@@ -12,8 +12,8 @@ module ApplicationHelper
     render partial: "form_errors", locals: {object: object}
   end
 
-  def form_text_field(f, tag, value='')
-    render partial: "form_text_field", locals: {f: f, tag: tag, value: value}
+  def form_text_field(f, tag)
+    render partial: "form_text_field", locals: {f: f, tag: tag}
   end
 
   def form_text_area(f, tag)
@@ -24,16 +24,16 @@ module ApplicationHelper
     render partial: "form_fancy_text_area", locals: {f: f, tag: tag}
   end
 
-  def form_email_field(f, tag, value='')
-    render partial: "form_email_field", locals: {f: f, tag: tag, value: value}
+  def form_email_field(f, tag)
+    render partial: "form_email_field", locals: {f: f, tag: tag}
   end
 
   def form_date_field(f, tag, id, value)
     render partial: "form_date_field", locals: {f: f, tag: tag, id: id, value: value}
   end
 
-  def form_number_field(f, tag, value='')
-    render partial: "form_number_field", locals: {f: f, tag: tag, value: value}
+  def form_number_field(f, tag)
+    render partial: "form_number_field", locals: {f: f, tag: tag}
   end
 
   def form_collection_select(f, *args)
