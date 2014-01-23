879c2f03454ddea37678c40a0dbc4789070c0ad1
Tom Naessens <siloxtom@gmail.com>
2014-01-23 15:32:34 +0100

    Fix #293

--- Isengard/app/models/access_level.rb
+++ Isengard/app/models/access_level.rb
@@ -32,7 +32,7 @@ class AccessLevel < ActiveRecord::Base
     end
   end
 
-  default_scope order: "price, name"
+  default_scope { order "price, name" }
 
   def set_zones_by_ids zones
     self.zones = self.event.zones.find zones
