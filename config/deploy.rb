251ac9800aff16d3a2dfbef56e95812b42cc18e6
Toon Willems <willemstoon@gmail.com>
2014-02-05 11:45:12 +0100

    linked_files option is nice

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,20 +11,8 @@ set :repo_url, 'git@github.com:ZeusWPI/Gandalf.git'
 set :branch, 'feature/deploy-via-capistrano'
 set :deploy_to, '/home/gandalf/production'
 
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
 # Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/initializers/secret_token.rb}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
@@ -42,6 +30,7 @@ namespace :deploy do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
       # execute :touch, release_path.join('tmp/restart.txt')
+      invoke 'unicorn:restart'
     end
   end
 
