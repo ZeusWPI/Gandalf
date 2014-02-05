d3f6265b0e61c72793a8a056d06d286c55ac4309
Toon Willems <willemstoon@gmail.com>
2014-02-05 11:51:32 +0100

    fix issue with unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,11 +27,7 @@ namespace :deploy do
 
   desc 'Restart application'
   task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
-      invoke 'unicorn:restart'
-    end
+    invoke 'unicorn:restart'
   end
 
   after :publishing, :restart
