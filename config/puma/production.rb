2cd5ec42e0aa45afeeeba0c35b42727b1726826f
Toon Willems <willemstoon@gmail.com>
2014-02-05 17:46:17 +0100

    add puma as app server

---
+++ config/puma/production.rb
@@ -0,0 +1,20 @@
+threads 0,8
+workers 4
+
+environment 'production'
+daemonize
+pidfile tmp/puma.pid
+
+preload_app!
+
+bind 'unix://var/run/unicorn/puma.sock'
+
+on_worker_boot do
+  ActiveRecord::Base.connection_pool.disconnect!
+
+  ActiveSupport.on_load(:active_record) do
+    Activerecord::Base.establish_connection
+  end
+end
+
+
