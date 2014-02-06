6aef50a0dc56df5156886fb0d80d06db15e3028e
Tom Naessens <siloxtom@gmail.com>
2014-02-06 23:20:42 +0100

    Tests for the AL hidden toggle

--- test/helpers/access_levels_helper_test.rb
+++ test/helpers/access_levels_helper_test.rb
@@ -1,4 +1,12 @@
 require 'test_helper'
 
 class AccessLevelsHelperTest < ActionView::TestCase
+
+  test "should show correct hidden toggle" do
+    al = access_levels(:one)
+    assert visibility_icon(al).include? 'Hide'
+
+    al.hidden = true
+    assert visibility_icon(al).include? 'Show'
+  end
 end
