a815a16a191dc6b7ef8524396a0a1b00385899ba
Tom Naessens <siloxtom@gmail.com>
2014-02-13 19:23:14 +0100

    Absolutely no idea why this works but it does

--- app/helpers/registrations_helper.rb
+++ app/helpers/registrations_helper.rb
@@ -3,7 +3,7 @@ module RegistrationsHelper
   def nice_changeset(name, change)
     case name
     when 'paid'
-      "Changed the amount paid from #{euro(@registration.price - change[0])} to #{euro(@registration.price - change[1])}"
+      "Changed the amount paid from #{euro(@registration.price - change[0]/100)} to #{euro(@registration.price - change[1])}"
     when 'checked_in_at'
       "Checked in at #{nice_time change[1]}"
     when 'random_check'
