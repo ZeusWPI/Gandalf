e769df83b2c30d8c1cfd82adf9a7c8150340115a
Tom Naessens <siloxtom@gmail.com>
2014-02-13 11:40:01 +0100

    Paper_trail installation and attachment to the registrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140210141746) do
+ActiveRecord::Schema.define(version: 20140213103501) do
 
   create_table "access_levels", force: true do |t|
     t.string   "name"
@@ -189,6 +189,17 @@ ActiveRecord::Schema.define(version: 20140210141746) do
   end
 
   add_index "users", ["username"], name: "index_users_on_username", unique: true
+
+  create_table "versions", force: true do |t|
+    t.string   "item_type",  null: false
+    t.integer  "item_id",    null: false
+    t.string   "event",      null: false
+    t.string   "whodunnit"
+    t.text     "object"
+    t.datetime "created_at"
+  end
+
+  add_index "versions", ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
 
   create_table "zones", force: true do |t|
     t.string   "name"
