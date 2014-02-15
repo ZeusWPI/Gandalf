463186a0cba356ef08233f4c132986d867f561f1
Tom Naessens <siloxtom@gmail.com>
2014-02-15 15:35:06 +0100

    Fix the filters

--- app/grids/registrations_grid.rb
+++ app/grids/registrations_grid.rb
@@ -6,10 +6,10 @@ class RegistrationsGrid
   end
 
   # We use the lower() instead of ilike because SQLite dev doesn't like ilike
-  filter(:name) { |value| where("lower(?) like ?", :name, "%#{value.downcase}%") }
-  filter(:email) { |value| where("lower(?) like ?", :email, "%#{value.downcase}%") }
+  filter(:name) { |value| where("lower(registrations.name) like ?", "%#{value.downcase}%") }
+  filter(:email) { |value| where("lower(registrations.email) like ?", "%#{value.downcase}%") }
   filter(:access_level) { |value, scope| scope.joins(:access_levels).where(access_levels: { id: value }) }
-  filter(:payment_code) { |value| where("? like ?", :payment_code, "%#{value}%") }
+  filter(:payment_code) { |value| where("registrations.payment_code like ?","%#{value}%") }
   filter(:only_paid) { |value| where("paid = price")  if value == '1' }
   filter(:only_unpaid) { |value| where.not("paid = price")  if value == '1' }
 
