7afaed2dfff7609ebdd869a7fcdb40e02d0046c7
Toon Willems <willemstoon@gmail.com>
2014-02-16 19:42:11 +0100

    fixed and added tests for uniqueness check

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -25,9 +25,8 @@ class Registration < ActiveRecord::Base
 
   validates :name, presence: true
   validates :email, presence: true
-  validates :student_number, presence: true, format: {with: /\A[0-9]*\Z/, message: "has invalid format" }
+  validates :student_number, presence: true, format: {with: /\A[0-9]*\Z/, message: "has invalid format" }, uniqueness: { scope: :event }
   # one student_number per event
-  validates :student_number, uniqueness: { scope: :event }
   validates :paid, presence: true, numericality: { only_integer: true }
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :payment_code, presence: true, uniqueness: true
--- test/models/registration_test.rb
+++ test/models/registration_test.rb
@@ -3,7 +3,30 @@ require 'test_helper'
 class RegistrationTest < ActiveSupport::TestCase
   verify_fixtures Registration
 
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @r1 = Registration.new(student_number: "01", name: "test", email: "test@test.com", payment_code: Registration.create_payment_code, price: 1, paid: 0)
+    @r2 = @r1.dup
+    @r2.payment_code = Registration.create_payment_code
+  end
+
+  test "student_number should be unique on event basis" do
+    @r1.event = events(:codenight)
+    @r1.save!
+    @r2.event = events(:codenight)
+    assert !@r2.valid?
+    assert !@r2.errors[:student_number].blank?
+  end
+
+  test "student_number should work for multiple events" do
+    @r1.event = events(:codenight)
+    @r1.save!
+    @r2.event = events(:galabal)
+    assert @r2.save
+  end
+
+  def teardown
+    @r1.destroy
+    @r2.destroy
+  end
+
 end
