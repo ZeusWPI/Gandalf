ceedf0d6c7ef874e33c82dc56906f606c5450333
Tom Naessens <Tom.Naessens@UGent.be>
2014-03-07 17:58:44 +0100

    Added token generation to partners

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -2,13 +2,28 @@
 #
 # Table name: partners
 #
-#  id                   :integer          not null, primary key
-#  name                 :string(255)
-#  email                :string(255)
-#  authentication_token :string(255)
-#  created_at           :datetime
-#  updated_at           :datetime
+#  id                     :integer          not null, primary key
+#  name                   :string(255)
+#  email                  :string(255)
+#  authentication_token   :string(255)
+#  created_at             :datetime
+#  updated_at             :datetime
+#  encrypted_password     :string(255)      default(""), not null
+#  reset_password_token   :string(255)
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :string(255)
+#  last_sign_in_ip        :string(255)
 #
 
 class Partner < ActiveRecord::Base
+  acts_as_token_authenticatable
+
+  devise :timeoutable, :trackable
+
+  validates :name, uniqueness: true
+  validates :email, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140307154720) do
+ActiveRecord::Schema.define(version: 20140307163912) do
 
   create_table "access_levels", force: true do |t|
     t.string   "name"
@@ -117,9 +117,20 @@ ActiveRecord::Schema.define(version: 20140307154720) do
     t.string   "authentication_token"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
   end
 
   add_index "partners", ["authentication_token"], name: "index_partners_on_authentication_token"
+  add_index "partners", ["email"], name: "index_partners_on_email", unique: true
+  add_index "partners", ["reset_password_token"], name: "index_partners_on_reset_password_token", unique: true
 
   create_table "people", force: true do |t|
     t.string   "username"
