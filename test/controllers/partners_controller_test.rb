b7ae1fa2df250c723fd6c8216afa9def3ab967e6
Tom Naessens <Tom.Naessens@UGent.be>
2014-03-15 17:36:10 +0100

    Fixed the tests

--- test/controllers/partners_controller_test.rb
+++ test/controllers/partners_controller_test.rb
@@ -4,15 +4,16 @@ class PartnersControllerTest < ActionController::TestCase
   include Devise::TestHelpers
 
   def setup
-    sign_in users(:tom)
   end
 
   test "should get index" do
+    sign_in users(:tom)
     get :index, event_id: 1
     assert_response :success
   end
 
   test "should get show" do
+    sign_in partners(:KBC)
     get :show, event_id: 1, id: 1
     assert_response :success
   end
