f1e18e52da673c53954f4190650a9c4f080c91cb
Tom Naessens <Tom.Naessens@UGent.be>
2014-09-10 12:16:46 +0200

    Annotations

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: invitations
+#
+#  id              :integer          not null, primary key
+#  invitee_id      :integer
+#  inviter_id      :integer
+#  comment         :text
+#  paid            :integer
+#  price           :integer
+#  selected        :boolean          default(FALSE)
+#  accepted        :boolean          default(FALSE)
+#  access_level_id :integer
+#  created_at      :datetime
+#  updated_at      :datetime
+#
+
 
 class Invitation < ActiveRecord::Base
 
--- test/models/included_zone_test.rb
+++ test/models/included_zone_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: included_zones
+#
+#  id              :integer          not null, primary key
+#  zone_id         :integer
+#  access_level_id :integer
+#  created_at      :datetime
+#  updated_at      :datetime
+#
+
 require 'test_helper'
 
 class IncludedZoneTest < ActiveSupport::TestCase
