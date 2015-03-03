f4d295fb6d8c65719faf248e2dd050b170e7ae36
Tom Naessens <Tom.Naessens@UGent.be>
2015-03-03 13:34:04 +0100

    Add capistrano for king

---
+++ config/deploy/king.rb
@@ -0,0 +1,8 @@
+server 'king.ugent.be', user: 'gandalf', roles: %w{web app db},
+  ssh_options: {
+    forward_agent: true,
+    auth_methods: ['publickey'],
+    port: 2222
+  }
+
+set :rails_env, 'production'
