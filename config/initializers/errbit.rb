0d850248c4181f2bf6f11234cc88ab0167132586
Tom Naessens <Tom.Naessens@UGent.be>
2015-03-16 21:39:29 +0100

    Use ssl for errbit

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,5 +1,6 @@
 Airbrake.configure do |config|
     config.api_key = '47ef697722f25e7952911b731b975eda'
     config.host    = 'zeus-errbit.ilion.me'
-    config.port    = 80
+    config.port    = 443
+    config.secure  = config.port == 443
 end
