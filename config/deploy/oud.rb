c688804210d33dd5137aa9c39673b815fa7b8ca9
Tom Naessens <Tom.Naessens@UGent.be>
2015-07-30 19:28:30 +0200

    Also switch deploy envs

--- config/deploy/oud.rb
+++ config/deploy/oud.rb
@@ -6,5 +6,4 @@ server 'king.ugent.be', user: 'gandalf', roles: %w{web app db},
   }
 
 set :rails_env, 'production'
-set :rbenv_type, :system
-set :rbenv_ruby, File.read('.ruby-version').strip
+set :unicorn_pid, '/var/run/unicorn/gandalf.pid'
