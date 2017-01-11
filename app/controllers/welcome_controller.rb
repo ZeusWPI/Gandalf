c76902a6f4bffe1fd01a72bdbd0ddf12db43502f
Feliciaan De Palmenaer <feliciaan@de-palmenaer.eu>
2017-01-11 16:51:13 +0100

    remove unnecassary code

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    if Event.first
-      redirect_to event_path(Event.first)
-    end
+    redirect_to event_path(Event.first)
   end
 end
