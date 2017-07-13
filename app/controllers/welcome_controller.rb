466a0fe79880ad400fe237389907c922ddaaaee3
Feliciaan De Palmenaer <feliciaan@de-palmenaer.eu>
2017-07-13 22:05:32 +0200

    Updated some settings to start ticket sale foundation ball

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
   def index
     event = Event.find_by_id(5)
     if event != nil
-      redirect_to event_path(Event.first)
+      redirect_to event_path(event)
     end
   end
 end
