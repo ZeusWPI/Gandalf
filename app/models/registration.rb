d31fe6c29ba7e0a854c59a901efccb8dc60070c6
Tom Naessens <tom.naessens@mediagenix.tv>
2020-10-02 22:42:11 +0200

    Add translations, fix a lambda in the registration.rb

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -31,7 +31,7 @@ class Registration < ActiveRecord::Base
   validates :email, presence: true, email: true
   validates :student_number, format: {with: /\A[0-9]*\Z/, message: "has invalid format" },
     uniqueness: { scope: :event }, allow_blank: true
-  validates :student_number, presence: true, if: -> { "access_levels.first.try(:requires_login?)" }
+  validates :student_number, presence: true, if: -> { access_levels.first.try(:requires_login?) }
   validates :paid, presence: true, numericality: { only_integer: true }
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :payment_code, presence: true, uniqueness: true
