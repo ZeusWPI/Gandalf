0795eaceac4611e2c5c14dc19aeac006d46742b0
Tom Naessens <tom.naessens@mediagenix.tv>
2020-10-03 20:43:58 +0200

    Guess we need a timezone

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module Isengard
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Mussels from Brussels
+    config.time_zone = 'Brussels'
   end
 end
