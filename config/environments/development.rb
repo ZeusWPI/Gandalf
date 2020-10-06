20b9ee477157c7b4ada520a764fcb042dfaa8152
redfast00 <redfast00@gmail.com>
2020-10-06 05:53:48 +0200

    Add default_url_options config back for mails, set :sendmail as delivery_method

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,9 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
 
+  # Set base URL because emails don't have the Host header context
+  config.action_mailer.default_url_options = { :host => 'https://localhost:8080' }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,10 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :sendmail
+  # Set base URL because emails don't have the Host header context
+  config.action_mailer.default_url_options = { :host => 'https://event.fkgent.be' }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
