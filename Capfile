b9b8d93ea765d3e4010c8fc8602eabe4b76f2d84
Tom Naessens <silox@zeus.ugent.be>
2022-06-17 17:44:56 +0200

    Add copied_files task

--- Capfile
+++ Capfile
@@ -4,5 +4,8 @@ require 'capistrano/setup'
 # Includes default deployment tasks
 require 'capistrano/deploy'
 
+require "capistrano/scm/git"
+install_plugin Capistrano::SCM::Git
+
 require 'capistrano/docker'
 require 'capistrano/docker/migration'
--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :branch, 'capistrano-docker'
 set :deploy_to, '/home/gandalf/production'
 
 # Default value for :linked_files is []
-set :linked_files, %w{.env config/database.yml}
+set :linked_files, []
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
@@ -29,3 +29,38 @@ set :log_level, :debug
 set :docker_command, "podman"
 set :docker_compose, true
 set :docker_compose_command, "podman-compose"
+
+set :copied_files, %w{.env config/database.yml}
+
+module Capistrano
+  module DSL
+    module Paths
+      def copied_files(parent)
+        paths = fetch(:copied_files)
+        join_paths(parent, paths)
+      end
+
+      def copied_file_dirs(parent)
+        map_dirnames(copied_files(parent))
+      end
+    end
+  end
+end
+
+namespace :deploy do
+  namespace :check do
+    after :linked_files, :copied_files
+
+    task :copied_files do
+      on release_roles :all do
+        execute :mkdir, "-p", copied_file_dirs(release_path)
+
+        fetch(:copied_files).each do |file|
+          target = release_path.join(file)
+          source = shared_path.join(file)
+          execute :cp, source, target
+        end
+      end
+    end
+  end
+end
