64a59dcbe2549f3127c03227606b4b879b22e2cc
Tom Naessens <silox@zeus.ugent.be>
2022-10-17 20:06:08 +0200

    Any commit now, we'll have a working Glitchtip integration

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,5 +2,5 @@ Sentry.init do |config|
   config.dsn = ENV["SENTRY_DSN"]
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
   config.enabled_environments = %w[production]
-  config.debug = ENV.fetch(["SENTRY_DEBUG"], false)
+  config.debug = ENV.fetch("SENTRY_DEBUG", false)
 end
