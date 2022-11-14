25014c79d3620c1b460c92c803fa170618a2ed24
Tom Naessens <silox@zeus.ugent.be>
2022-11-14 21:42:14 +0100

    Add created_at to xls export

--- app/jobs/generate_registrations_xls_job.rb
+++ app/jobs/generate_registrations_xls_job.rb
@@ -11,9 +11,9 @@ class GenerateRegistrationsXlsJob < ApplicationJob
     xls = Spreadsheet::Workbook.new
 
     sheet = xls.create_worksheet
-    sheet.update_row 0, "Naam", "Email", "Studentnummer", "Ticket", "Comment", "Te betalen"
+    sheet.update_row 0, "Datum", "Naam", "Email", "Studentnummer", "Ticket", "Comment", "Te betalen"
     event.registrations.each.with_index do |reg, i|
-      sheet.update_row i + 1, reg.name, reg.email, reg.student_number, reg.access_level.name, reg.comment, reg.to_pay
+      sheet.update_row i + 1, reg.created_at, reg.name, reg.email, reg.student_number, reg.access_level.name, reg.comment, reg.to_pay
     end
 
     file = Tempfile.new
