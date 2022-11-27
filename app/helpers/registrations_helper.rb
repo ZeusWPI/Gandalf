cc08862dd12fe4bba353ad2bf1332e4ddc70c9a5
Tom Naessens <silox@zeus.ugent.be>
2022-11-27 21:10:49 +0100

    Okay rubycop

--- app/helpers/registrations_helper.rb
+++ app/helpers/registrations_helper.rb
@@ -4,7 +4,7 @@ module RegistrationsHelper
   def nice_changeset(name, change, registration_price)
     case name
     when 'paid'
-      "Changed the amount paid from #{euro(registration_price - (change[0] / 100))} to #{euro(registration_price - change[1] / 100)}"
+      "Changed the amount paid from #{euro(registration_price - (change[0] / 100))} to #{euro(registration_price - (change[1] / 100)}"
     when 'checked_in_at'
       "Checked in at #{nice_time change[1]}"
     when 'payment_code'
