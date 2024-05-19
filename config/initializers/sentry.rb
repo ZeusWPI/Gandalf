3d2f283e32ae47a4951465992c18b02cc3b5555f
Tom Naessens <silox@zeus.ugent.be>
2024-05-19 17:57:09 +0200

    Sentry part III

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,4 +2,5 @@ Sentry.init do |config|
   config.dsn = ENV["SENTRY_DSN"]
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
   config.enable_tracing = true
+  config.enabled_environments = %w[production]
 end
